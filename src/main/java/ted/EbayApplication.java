a22d5bea7442ecdf4347ce6c5265c669072714c3
geooo109 <sdi1400109@di.uoa.gr>
2019-07-20 22:23:01 +0300

    removed un/ry imports

--- src/main/java/ted/EbayApplication.java
+++ src/main/java/ted/EbayApplication.java
@@ -15,8 +15,6 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.stereotype.Component;
-import ted.repository.UserRepository;
-import ted.model.*;
 import ted.service.UserService;
 
 import java.util.List;
