ff7a12f85f6e27c059cf2c206a0294b0cc5756a1
geooo109 <sdi1400109@di.uoa.gr>
2019-07-31 18:29:11 +0300

    /app change

--- src/main/java/com/dit/ebay/security/SecurityConstants.java
+++ src/main/java/com/dit/ebay/security/SecurityConstants.java
@@ -5,6 +5,6 @@ public class SecurityConstants {
     public static final long   EXPIRATION_TIME = 259_200_000; // 3 days
     public static final String TOKEN_PREFIX = "Bearer ";
     public static final String HEADER = "Authorization";
-    public static final String SIGN_UP_URL = "/api/users";
-    public static final String SIGN_IN_URL = "/api/signin";
+    public static final String SIGN_UP_URL = "/app/users";
+    public static final String SIGN_IN_URL = "/app/signin";
 }
