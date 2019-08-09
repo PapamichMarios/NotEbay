ddb1ad4a7d89abf1a2b2c9b14d5b188e3cb4e9c2
PapamichMarios <papamichalopoulos.m@gmail.com>
2019-08-10 01:11:06 +0300

    Redesigned signup with steps

---
+++ src/main/resources/static/reactjs/components/signup/accountDetails.js
@@ -0,0 +1,164 @@
+import React from 'react';
+
+import { Formik } from 'formik';
+import * as Yup from 'yup';
+
+import { ButtonGroup, ButtonToolbar, Container, Row, Col, Button, Card, InputGroup, Alert, Form } from 'react-bootstrap';
+import { FaUser, FaLock, FaEnvelope, FaPhone, FaHome, FaGlobe, FaFile } from 'react-icons/fa';
+
+export default class AccountDetails extends React.Component{
+    constructor(props) {
+        super(props);
+
+        this.saveAndContinue = this.saveAndContinue.bind(this);
+    }
+
+    saveAndContinue(e) {
+        this.props.nextStep();
+    }
+
+    render(){
+        const AccountDetailsSchema = Yup.object({
+            username: Yup.string().min(4, 'Too short!').max(30, 'Too long!').required(),
+            email: Yup.string().email('Invalid email address').min(4, 'Too short!').max(30, 'Too long!').required(),
+            password: Yup.string().min(4, 'Too short!').max(30, 'Too long!').required(),
+            repassword: Yup.string().min(4, 'Too short!').max(30, 'Too long!').required()
+        });
+
+        return (
+            <Container>
+                <Card border="dark">
+                  <Card.Header as="h3" className="text-center bg-dark" style={{color:'white'}}> Signup </Card.Header>
+                  <Card.Body>
+                    <Card.Title className="text-center"> Enter your account details. </Card.Title>
+                    <Formik
+                        initialValues={{
+                            username: this.props.values.username,
+                            email: this.props.values.email,
+                            password: this.props.values.password,
+                            repassword: this.props.values.repassword
+                        }}
+                        validationSchema={AccountDetailsSchema}
+                        onSubmit={this.saveAndContinue}
+                    >
+                    {({
+                        handleSubmit,
+                        handleChange,
+                        handleBlur,
+                        isValid,
+                        isInvalid,
+                        errors,
+                        touched,
+                        values
+                    }) => (
+                        <Form noValidate onSubmit={handleSubmit}>
+                            <Form.Row>
+                                <Col>
+                                    <Form.Group controlId="formUsername">
+                                      <InputGroup>
+                                        <InputGroup.Prepend>
+                                          <InputGroup.Text> <FaUser/> </InputGroup.Text>
+                                        </InputGroup.Prepend>
+                                        <Form.Control
+                                            type="text"
+                                            name="username"
+                                            placeholder="Username"
+                                            onChange={e => {
+                                                handleChange(e)
+                                                this.props.onChange(e);
+                                            }}
+                                            value={values.username}
+                                            onBlur={handleBlur}
+                                            isValid={!errors.username && touched.username}
+                                            isInvalid={errors.username && touched.username}
+                                            />
+                                        <Form.Control.Feedback type="invalid"> {errors.username}</Form.Control.Feedback>
+                                      </InputGroup>
+                                    </Form.Group>
+
+                                    <Form.Group controlId="emailForm">
+                                      <InputGroup>
+                                        <InputGroup.Prepend>
+                                          <InputGroup.Text> <FaEnvelope/> </InputGroup.Text>
+                                        </InputGroup.Prepend>
+                                        <Form.Control
+                                            type="email"
+                                            name="email"
+                                            placeholder="E-mail"
+                                            onChange={e => {
+                                                handleChange(e)
+                                                this.props.onChange(e);
+                                            }}
+                                            value={values.email}
+                                            onBlur={handleBlur}
+                                            isValid={!errors.email && touched.email}
+                                            isInvalid={errors.email && touched.email}
+                                             />
+                                        <Form.Control.Feedback type="invalid"> {errors.email} </Form.Control.Feedback>
+                                      </InputGroup>
+
+                                      <Form.Text className="text-muted">
+                                        We'll never share your email with anyone else.
+                                      </Form.Text>
+                                    </Form.Group>
+
+                                    <Form.Group controlId="formPassword">
+                                      <InputGroup>
+                                        <InputGroup.Prepend>
+                                          <InputGroup.Text> <FaLock/> </InputGroup.Text>
+                                        </InputGroup.Prepend>
+                                        <Form.Control
+                                            type="password"
+                                            name="password"
+                                            placeholder="Password"
+                                            value={values.password}
+                                            onChange={e => {
+                                                handleChange(e)
+                                                this.props.onChange(e);
+                                            }}
+                                            onBlur={handleBlur}
+                                            isValid={!errors.password && touched.password}
+                                            isInvalid={errors.password && touched.password}
+                                            />
+                                        <Form.Control.Feedback type="invalid"> {errors.password} </Form.Control.Feedback>
+                                      </InputGroup>
+                                    </Form.Group>
+
+                                    <Form.Group controlId="formRepeatPassword">
+                                      <InputGroup>
+                                        <InputGroup.Prepend>
+                                          <InputGroup.Text> <FaLock/> </InputGroup.Text>
+                                        </InputGroup.Prepend>
+                                        <Form.Control
+                                            type="password"
+                                            name="repassword"
+                                            placeholder="Re-enter Password"
+                                            value={values.repassword}
+                                            onChange={e => {
+                                                handleChange(e)
+                                                this.props.onChange(e);
+                                            }}
+                                            onBlur={handleBlur}
+                                            isValid={!errors.repassword && touched.repassword}
+                                            isInvalid={errors.repassword && touched.repassword}
+                                            />
+                                        <Form.Control.Feedback type="invalid"> {errors.repassword} </Form.Control.Feedback>
+                                      </InputGroup>
+                                    </Form.Group>
+                                </Col>
+                            </Form.Row>
+
+                            <Form.Row>
+                                <Col md={{offset: 10}}>
+                                    <Button type="submit" variant="success" block> Save and Continue </Button>
+                                </Col>
+                            </Form.Row>
+                        </Form>
+                    )}
+                    </Formik>
+                  </Card.Body>
+                </Card>
+            </Container>
+        );
+    }
+}
