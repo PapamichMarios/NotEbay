719ff8a7dd81c98584e256200cb26c1ecf4e9069
geooo109 <sdi1400109@di.uoa.gr>
2019-08-27 03:49:50 +0300

    enable cron

--- src/main/java/com/dit/ebay/component/ItemTimeScheduleComponent.java
+++ src/main/java/com/dit/ebay/component/ItemTimeScheduleComponent.java
@@ -7,6 +7,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Async;
+import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
@@ -18,6 +19,7 @@ import java.util.List;
  */
 
 @Component
+@EnableScheduling
 public class ItemTimeScheduleComponent {
 
     private static final Logger log = LoggerFactory.getLogger(ItemTimeScheduleComponent.class);
